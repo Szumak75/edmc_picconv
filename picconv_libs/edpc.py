3b2d5f0dd1e4b5c182f7eb4dca1fd135a1264a5f
Jacek Kotlarski <szumak@virthost.pl>
2024-01-14 18:03:32 +0100

    edpc module

---
+++ picconv_libs/edpc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+  edpc.py
+  Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+  Created: 14.01.2024, 18:01:41
+  
+  Purpose: 
+"""
+
+import sys
+
+
+if __name__ == "__main__":
+    sys.exit(0)
+
+# #[EOF]#######################################################################
